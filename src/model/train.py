29a073c8eb8b4a33010c530d00b20abfd37d698e
msuribec <37965605+msuribec@users.noreply.github.com>
2025-09-21 00:58:31 +0000

    añade train

--- src/model/train.py
+++ src/model/train.py
@@ -9,111 +9,6 @@ def read(data_dir, split):
     return df
 
 
-def train(model, train_loader, valid_loader, config):
-    optimizer = getattr(torch.optim, config.optimizer)(model.parameters())
-    model.train()
-    example_ct = 0
-    for epoch in range(config.epochs):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = data.to(device), target.to(device)
-            data = data.view(data.shape[0],-1)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            loss.backward()
-            optimizer.step()
-
-            example_ct += len(data)
-
-            if batch_idx % config.batch_log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0%})]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    batch_idx / len(train_loader), loss.item()))
-                
-                train_log(loss, example_ct, epoch)
-
-        # evaluate the model on the validation set at each epoch
-        loss, accuracy = test(model, valid_loader)  
-        test_log(loss, accuracy, example_ct, epoch)
-
-    
-def test(model, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            data = data.view(data.shape[0],-1)
-            output = model(data)
-            test_loss += F.cross_entropy(output, target, reduction='sum')  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum()
-
-    test_loss /= len(test_loader.dataset)
-
-    accuracy = 100. * correct / len(test_loader.dataset)
-    
-    return test_loss, accuracy
-
-
-def train_log(loss, example_ct, epoch):
-    loss = float(loss)
-    # where the magic happens
-    wandb.log({"epoch": epoch, "train/loss": loss}, step=example_ct)
-    print(f"Loss after " + str(example_ct).zfill(5) + f" examples: {loss:.3f}")
-    
-
-def test_log(loss, accuracy, example_ct, epoch):
-    loss = float(loss)
-    accuracy = float(accuracy)
-    # where the magic happens
-    wandb.log({"epoch": epoch, "validation/loss": loss, "validation/accuracy": accuracy}, step=example_ct)
-    print(f"Loss/accuracy after " + str(example_ct).zfill(5) + f" examples: {loss:.3f}/{accuracy:.3f}")
-
-def evaluate(model, test_loader):
-    """
-    ## Evaluate the trained model
-    """
-
-    loss, accuracy = test(model, test_loader)
-    highest_losses, hardest_examples, true_labels, predictions = get_hardest_k_examples(model, test_loader.dataset)
-
-    return loss, accuracy, highest_losses, hardest_examples, true_labels, predictions
-
-def get_hardest_k_examples(model, testing_set, k=32):
-    model.eval()
-
-    loader = DataLoader(testing_set, 1, shuffle=False)
-
-    # get the losses and predictions for each item in the dataset
-    losses = None
-    predictions = None
-    with torch.no_grad():
-        for data, target in loader:
-            data, target = data.to(device), target.to(device)
-            data = data.view(data.shape[0],-1)
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            pred = output.argmax(dim=1, keepdim=True)
-            
-            if losses is None:
-                losses = loss.view((1, 1))
-                predictions = pred
-            else:
-                losses = torch.cat((losses, loss.view((1, 1))), 0)
-                predictions = torch.cat((predictions, pred), 0)
-
-    argsort_loss = torch.argsort(losses, dim=0).cpu()
-
-    highest_k_losses = losses[argsort_loss[-k:]]
-    hardest_k_examples = testing_set[argsort_loss[-k:]][0]
-    true_labels = testing_set[argsort_loss[-k:]][1]
-    predicted_labels = predictions[argsort_loss[-k:]]
-
-    return highest_k_losses, hardest_k_examples, true_labels, predicted_labels
-
-from torch.utils.data import DataLoader
 
 def train_and_log(config,experiment_id='99'):
 
@@ -134,62 +29,30 @@ def train_and_log(config,experiment_id='99'):
 
         model_artifact = run.use_artifact("RandomForest:latest")
         model_dir = model_artifact.download()
-        model_path = os.path.join(model_dir, "initialized_model_RandomForest.pkl")
+        model_path = os.path.join(model_dir, "model/initialized_model_RandomForest.pkl")
         model_config = model_artifact.metadata
         config.update(model_config)
 
-        model = rfClassifier(model_config)
-        model.load_state_dict(torch.load(model_path))
-        model = model.to(device)
+
+
+        # model = rfClassifier(model_config)
+        # model.load_state_dict(torch.load(model_path))
+        # model = model.to(device)
  
-        train(model, train_loader, validation_loader, config)
+        # train(model, train_loader, validation_loader, config)
 
-        model_artifact = wandb.Artifact(
-            "trained-model", type="model",
-            description="Trained NN model",
-            metadata=dict(model_config))
+        # model_artifact = wandb.Artifact(
+        #     "trained-model", type="model",
+        #     description="Trained NN model",
+        #     metadata=dict(model_config))
 
-        torch.save(model.state_dict(), "trained_model.pth")
-        model_artifact.add_file("trained_model.pth")
-        wandb.save("trained_model.pth")
+        # torch.save(model.state_dict(), "trained_model.pth")
+        # model_artifact.add_file("trained_model.pth")
+        # wandb.save("trained_model.pth")
 
         run.log_artifact(model_artifact)
 
     return model
-
-    
-def evaluate_and_log(experiment_id='99',config=None,):
-
-    
-    seed = int(os.environ["SEED"])
-    project_name = os.environ["PROJECT_NAME"]
-    dataset_name = os.environ["DATASET_NAME"]
-
-
-    with wandb.init(project=project_name, name=f"Eval Model ExecId-{args.IdExecution} ExperimentId-{experiment_id}", job_type="train-model", config=config) as run:
-        data = run.use_artifact(f'{dataset_name}-preprocess:latest')
-        data_dir = data.download()
-        testing_set = read(data_dir, "test")
-
-        test_loader = torch.utils.data.DataLoader(testing_set, batch_size=128, shuffle=False)
-
-        model_artifact = run.use_artifact("trained-model:latest")
-        model_dir = model_artifact.download()
-        model_path = os.path.join(model_dir, "trained_model.pth")
-        model_config = model_artifact.metadata
-
-        model = Classifier(**model_config)
-        model.load_state_dict(torch.load(model_path))
-        model.to(device)
-
-        loss, accuracy, highest_losses, hardest_examples, true_labels, preds = evaluate(model, test_loader)
-
-        run.summary.update({"loss": loss, "accuracy": accuracy})
-
-        wandb.log({"high-loss-examples":
-            [wandb.Image(hard_example, caption=str(int(pred)) + "," +  str(int(label)))
-             for hard_example, pred, label in zip(hardest_examples, preds, true_labels)]})
-
 
 if name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -201,12 +64,4 @@ if name__ == "__main__":
     else:
         args.IdExecution = "testing console"
 
-    model = train_and_log(train_config,id)
-    evaluate_and_log(id)        
-
-    """    
-    train_config = {}
-
-    model = train_and_log(train_config)
-    evaluate_and_log()
-    """
+    model = train_and_log(train_config, id)       
